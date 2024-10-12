41afad168f3eb93b27fad9e133e624f4f54250ac
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-10-13 00:11:17 +0300

    Solution fix

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import json
 from decimal import Decimal
+from pathlib import Path
 
 
 def calculate_profit(filename: str) -> None:
@@ -12,30 +13,34 @@ def calculate_profit(filename: str) -> None:
     Returns:
         None
     """
+    root_dir = Path(__file__).resolve().parent.parent
+    profit_file_path = root_dir / "profit.json"
+
     with open(filename, "r") as file:
         trades = json.load(file)
 
-    earned_money = Decimal("0")
+    total_earned_money = Decimal("0")
     matecoin_account = Decimal("0")
 
     for trade in trades:
+        bought = trade["bought"]
+        sold = trade["sold"]
         price = Decimal(trade["matecoin_price"])
 
-        if trade["bought"]:
-            bought = Decimal(trade["bought"])
-            matecoin_account += bought
-            earned_money -= bought * price
-        elif trade["sold"]:
-            sold = Decimal(trade["sold"])
-            matecoin_account -= sold
-            earned_money += sold * price
+        if bought:
+            matecoin_account += Decimal(bought)
+            total_earned_money -= price * Decimal(bought)
 
-    result = {
-        "earned_money": str(earned_money),
-        "matecoin_account": str(matecoin_account)
+        if sold:
+            matecoin_account -= Decimal(sold)
+            total_earned_money += price * Decimal(sold)
+
+    profit_data = {
+        "earned_money": str(total_earned_money.normalize()),
+        "matecoin_account": str(matecoin_account.normalize())
     }
 
-    with open("profit.json", "w") as file:
-        json.dump(result, file, indent=2)
+    with open(profit_file_path, "w") as profit_file:
+        json.dump(profit_data, profit_file, indent=2)
 
     print("The profit.json file was successfully created.")
